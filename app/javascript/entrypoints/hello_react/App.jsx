0e5b56ff5d6aa13690432b9ea78132689f4cdb53
Maurizio De Santis <desantis.maurizio@gmail.com>
2021-12-11 20:04:23 +0100

    skip navigation on pages with same component

--- app/javascript/entrypoints/hello_react/App.jsx
+++ app/javascript/entrypoints/hello_react/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react'
 import logoSeagull from './logoSeagull.svg'
 import logoEnergyYellow from './logoEnergyYellow.svg'
+import { navigator } from '@hotwired/turbo'
 import './App.css'
 
-function App({ logoColor }) {
+function App(props) {
   const [count, setCount] = useState(0)
+  const [logoColor, setLogoColor] = useState(props.logoColor)
 
-  const logo = logoColor === 'energyYellow' ? logoEnergyYellow : logoSeagull
+  const handleChangeLogoLinks = (event, logoColor) => {
+    event.preventDefault()
+    setLogoColor(logoColor)
+    navigator.history.push(new URL(event.target.href))
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoColor === 'energyYellow' ? logoEnergyYellow : logoSeagull} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
@@ -25,6 +31,7 @@ function App({ logoColor }) {
           <a
             className="App-link seagull"
             href="/hello_react"
+            onClick={(e) => handleChangeLogoLinks(e, 'seagull')}
           >
             Logo Seagull
           </a>
@@ -32,6 +39,7 @@ function App({ logoColor }) {
           <a
             className="App-link energyYellow"
             href="/hello_react_2"
+            onClick={(e) => handleChangeLogoLinks(e, 'energyYellow')}
           >
             Logo Energy Yellow
           </a>

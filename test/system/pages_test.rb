268cd0647f01caf494544d77e59f9ff5a32d6d85
Maurizio De Santis <desantis.maurizio@gmail.com>
2021-12-18 18:52:02 +0100

    correct rubocop safe offenses

--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -1,43 +1,43 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class PagesTest < ApplicationSystemTestCase
   setup do
     @page = pages(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit pages_url
-    assert_selector "h1", text: "Pages"
+    assert_selector 'h1', text: 'Pages'
   end
 
-  test "should create page" do
+  test 'should create page' do
     visit pages_url
-    click_on "New page"
+    click_on 'New page'
 
-    fill_in "Author", with: @page.author
-    fill_in "Content", with: @page.content
-    click_on "Create Page"
+    fill_in 'Author', with: @page.author
+    fill_in 'Content', with: @page.content
+    click_on 'Create Page'
 
-    assert_text "Page was successfully created"
-    click_on "Back"
+    assert_text 'Page was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Page" do
+  test 'should update Page' do
     visit page_url(@page)
-    click_on "Edit this page", match: :first
+    click_on 'Edit this page', match: :first
 
-    fill_in "Author", with: @page.author
-    fill_in "Content", with: @page.content
-    click_on "Update Page"
+    fill_in 'Author', with: @page.author
+    fill_in 'Content', with: @page.content
+    click_on 'Update Page'
 
-    assert_text "Page was successfully updated"
-    click_on "Back"
+    assert_text 'Page was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Page" do
+  test 'should destroy Page' do
     visit page_url(@page)
-    click_on "Destroy this page", match: :first
+    click_on 'Destroy this page', match: :first
 
-    assert_text "Page was successfully destroyed"
+    assert_text 'Page was successfully destroyed'
   end
 end

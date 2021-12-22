afe7dd9699f23378bf475c95e98b0d79d5129354
Maurizio De Santis <desantis.maurizio@gmail.com>
2021-12-22 23:30:02 +0100

    add admin social accounts link to navigation drawer

--- app/javascript/components/layouts/admin/_drawer.jsx
+++ app/javascript/components/layouts/admin/_drawer.jsx
@@ -5,18 +5,16 @@
 import {
   AppBar,
   Box,
-  // Button,
-  // Divider,
-  // List,
-  // ListItem,
-  // ListItemIcon,
-  // ListItemText,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
   Drawer as MuiDrawer,
   Toolbar,
   Typography
 } from '@mui/material'
 
-// import { MoveToInbox as InboxIcon, Mail as MailIcon } from '@mui/icons-material'
+import { adminSocialAccountsPath } from '/routes'
 
 const drawerWidth = 240
 
@@ -41,23 +39,13 @@ export default function Drawer(props) {
       >
         <Toolbar />
         <Box sx={{ 'overflow': 'auto' }}>
-          {/* <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>)}
-          </List>
-          <Divider />
           <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>)}
-          </List> */}
+            <ListItem disablePadding>
+              <ListItemButton component="a" href={adminSocialAccountsPath()}>
+                <ListItemText primary="Social Accounts" />
+              </ListItemButton>
+            </ListItem>
+          </List>
         </Box>
       </MuiDrawer>
       <Box component="main" sx={{ 'flexGrow': 1, 'p': 3 }}>

f35b05957d8bb71dddc893ef65c79bd38b0362f5
Maurizio De Santis <desantis.maurizio@gmail.com>
2021-12-26 18:56:27 +0100

    allow setting latency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,23 @@ class ApplicationController < ActionController::Base
 
   layout 'hello_react', only: %i[hello_react hello_react_2]
 
+  before_action :handle_latency
+
   def hello_react
   end
 
   def hello_react_2
+  end
+
+  private
+
+  def handle_latency
+    sleep session[:latency] if session[:latency]&.positive?
+
+    return unless (latency = params[:latency])
+
+    latency = latency.to_f
+
+    session[:latency] = latency
   end
 end

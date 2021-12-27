4c17ff3133dda4aa1cefa9457527f544f81d7492
Maurizio De Santis <desantis.maurizio@gmail.com>
2021-12-27 20:40:47 +0100

    decouple form from dialog

--- app/javascript/components/admin/social_accounts/_new_dialog_form.jsx
+++ app/javascript/components/admin/social_accounts/_new_dialog_form.jsx
@@ -9,7 +9,6 @@ import {
   TextField
 } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
-import { useTheme } from '@mui/material/styles'
 
 import AuthenticityTokenField from '/components/application/_authenticity_token_field'
 
@@ -52,10 +51,10 @@ function FormController(props) {
   )
 }
 
-export default function NewDialogForm(props) {
-  const { 'onClose': onDialogClose, 'open': dialogOpen, socialAccount } = props
-  const theme = useTheme()
-  const { control, 'formState': { 'errors': formErrors }, reset, trigger } = useForm({
+function Form(props) {
+  const { onDialogClose, socialAccount } = props
+
+  const { control, 'formState': { 'errors': formErrors }, trigger } = useForm({
     'defaultValues': {
       'social_account[app_id]': socialAccount.credentials?.appId ?? '',
       'social_account[app_secret]': socialAccount.credentials?.appSecret ?? '',
@@ -68,54 +67,52 @@ export default function NewDialogForm(props) {
     useStopFormSubmissionOnClientSideValidationFailed(trigger)
   const formErrorsPresent = Object.keys(formErrors).length !== 0
 
-  const resetFormAfterDialogTransitionEnd = () => setTimeout(() => reset(), theme.transitions.duration.standard)
-  const handleDialogClose = (event) => {
-    resetFormAfterDialogTransitionEnd()
-
-    if (onDialogClose) onDialogClose(event)
-  }
-
   const [useFormSubmittingRef, submitting] = useFormSubmitting()
 
   const formRef = composeRefs(useStopFormSubmissionOnClientSideValidationFailedRef, useFormSubmittingRef)
 
   return (
+    <form action={adminSocialAccountsPath()} ref={formRef} acceptCharset="utf-8" method="post" data-remote>
+      <AuthenticityTokenField />
+      <DialogTitle>New Social Account</DialogTitle>
+      <DialogContent>
+        <input type="hidden" name="social_account[type]" value={socialAccount.type} />
+        <FormController
+          control={control}
+          name="social_account[name]"
+          label="Name *"
+          rules={{ 'required': 'Please insert a damn name' }} />
+        <FormController
+          control={control}
+          name="social_account[app_id]"
+          label="Facebook App Id *"
+          rules={{ 'required': 'Please insert a bloody Facebook App Id' }} />
+        <FormController
+          control={control}
+          name="social_account[app_secret]"
+          label="Facebook App Secret *"
+          rules={{ 'required': 'Please insert a motherfucking Facebook App Secret' }} />
+      </DialogContent>
+      <DialogActions>
+        <Button type="button" onClick={onDialogClose}>Cancel</Button>
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          disabled={formErrorsPresent}
+          loading={submitting}
+        >Create</LoadingButton>
+      </DialogActions>
+    </form>
+  )
+}
+
+export default function NewDialogForm(props) {
+  const { 'onClose': onDialogClose, 'open': dialogOpen, socialAccount } = props
+
+  return (
     <div>
-      <Dialog open={dialogOpen} onClose={handleDialogClose}>
-        <form action={adminSocialAccountsPath()} ref={formRef} acceptCharset="utf-8" method="post" data-remote>
-          <AuthenticityTokenField />
-          <DialogTitle>New Social Account</DialogTitle>
-          <DialogContent>
-            <input type="hidden" name="social_account[type]" value={socialAccount.type} />
-            <FormController
-              control={control}
-              name="social_account[name]"
-              label="Name *"
-              rules={{ 'required': 'Please insert a damn name' }}
-            />
-            <FormController
-              control={control}
-              name="social_account[app_id]"
-              label="Facebook App Id *"
-              rules={{ 'required': 'Please insert a bloody Facebook App Id' }}
-            />
-            <FormController
-              control={control}
-              name="social_account[app_secret]"
-              label="Facebook App Secret *"
-              rules={{ 'required': 'Please insert a motherfucking Facebook App Secret' }}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button type="button" onClick={handleDialogClose}>Cancel</Button>
-            <LoadingButton
-              type="submit"
-              variant="contained"
-              disabled={formErrorsPresent}
-              loading={submitting}
-            >Create</LoadingButton>
-          </DialogActions>
-        </form>
+      <Dialog open={dialogOpen} onClose={onDialogClose}>
+        <Form socialAccount={socialAccount} onDialogClose={onDialogClose} />
       </Dialog>
     </div>
   )

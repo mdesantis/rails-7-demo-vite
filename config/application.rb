f5aefc137c4abfd1be7a220eeba977ae1c047ef7
Maurizio De Santis <desantis.maurizio@gmail.com>
2022-01-01 13:35:18 +0100

    disable including all helpers into controllers

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Rails7DemoVite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_controller.include_all_helpers = false
   end
 end
